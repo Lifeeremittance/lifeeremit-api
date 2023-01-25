f8ccb4aa606b409c1f2c323fd6c546504989525f
Alan Douglas <aland6209@gmail.com>
2023-01-26 00:30:26 +0100

    chore: allow user sign up with full name

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -4,11 +4,6 @@ import { CreateUserDto } from "./create-user.dto";
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
-  @Length(6, 30)
-  @IsOptional()
-  readonly fullName: string;
-
-  @IsString()
   @Length(6, 6)
   @IsOptional()
   readonly token: string;
